a444ea5adb1767493c17e6e91c21adb78ec80042
niha siddiqui <niha.siddiqui@informatik.hs-fulda.de>
2026-06-25 17:40:36 +0200

    learning landing page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,7 @@
             @endif
 
             <x-primary-button class="ms-3">
-                {{ __('Log in as Parent') }}
+                {{ __('Login') }}
             </x-primary-button>
         </div>
     </form>
